ea845d4c6df948590687315a4a6813464ee29b9e
Daniel P. <147325072+dani238qr@users.noreply.github.com>
2024-09-30 13:12:51 +0300

    Update windows_src.c

--- src/windows_src.c
+++ src/windows_src.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
+
+#define INTERVAL 60
+#define IP_LOG_FILE "url\\ip.log"
+#define URL_LOG_FILE "url\\url.log"
+
+void check_website(const char *url) {
+      char log_message_buf[512];
+  
+}
+
+time_t now = time(NULL);
+struct tm *t = localtime(&now);
+char time_str[100];
+strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", t);
+
+int main(){
+
+  check_website("http://www.google.com");
+  
+  return 0;
+}
