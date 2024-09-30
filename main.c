8d2050dc7af5375b0a8132dcc0624b9c5a166f3e
Daniel P. <147325072+dani238qr@users.noreply.github.com>
2024-09-30 13:13:48 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+    #include <windows.h>
+    #define OS_WINDOWS 1
+    #define WAIT_TIME 2000
+#else
+    #include <unistd.h>
+    #define OS_WINDOWS 0
+    #define WAIT_TIME 1
+#endif
+
 
