b036085791fdfb239b5783becf1d26bf963b70e6
Daniel P. <147325072+dani238qr@users.noreply.github.com>
2024-10-16 20:31:50 +0300

    Update main.c

--- main.c
+++ main.c
@@ -20,17 +20,35 @@ int main(int argc, char *argv[]) {
     }
 
     if (OS_WINDOWS) {
-        printf("Windows\n");
+        system("gcc -o windows_monitor.exe src\\windows_src.c -lcurl"); 
     }
     else{
         system("chmod +x src/linux_src.sh");
     }
 
+    #if OS_WINDOWS
+        Sleep(WAIT_TIME);
+    #endif
 
-        if (OS_WINDOWS) {
-            printf("WINDOES\n");
+    if (OS_WINDOWS) {
+        if (strcmp(argv[1],"h")==0){
+            system("windows_monitor.exe h" );
+        }
+        else if (strcmp(argv[1],"web")==0){
+        system("windows_monitor.exe web" );
+        }
+        else if (strcmp(argv[1],"ip")==0){
+            system("windows_monitor.exe ip" );
+        }
+        else if (strcmp(argv[1],"all")==0){
+            system("windows_monitor.exe all" );
+        }
+        else{
+            fprintf(stderr, "add h for help\n");
+            return 1;
+        }
+
     }
-            
     else {
         if (strcmp(argv[1],"h")==0){
             system("./src/linux_src.sh h");
@@ -52,5 +70,6 @@ int main(int argc, char *argv[]) {
     }
 
     return 0;
-
 }
+
+
